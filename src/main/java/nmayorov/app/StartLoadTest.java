377db0cb2d34e23dfb98661af34d482697307bcc
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2018-03-30 15:42:22 +0500

    Add StartLoadTest

---
+++ src/main/java/nmayorov/app/StartLoadTest.java
@@ -0,0 +1,28 @@
+package nmayorov.app;
+
+import nmayorov.client.Client;
+import nmayorov.client.ConsoleDisplay;
+import nmayorov.client.SpamBot;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class StartLoadTest {
+    private static final int PORT = 5000;
+    private static int DEFAULT_BOT_COUNT = 200;
+
+    public static void main(String[] args) throws IOException {
+        int botCount;
+        if (args.length == 2) {
+            botCount = Integer.valueOf(args[1]);
+        } else {
+            botCount = DEFAULT_BOT_COUNT;
+        }
+        for (int i = 0; i < botCount; ++i) {
+            Client c = new Client(new SpamBot(), new ConsoleDisplay());
+            c.connect(new InetSocketAddress(PORT));
+            Thread t = new Thread(c);
+            t.start();
+        }
+    }
+}
