28e6750e7fbee9979e0c65438c9e830b48056e52
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2018-04-05 05:02:54 +0500

    Use put method for adding inbound connections

--- src/main/java/nmayorov/server/ConnectionAcceptor.java
+++ src/main/java/nmayorov/server/ConnectionAcceptor.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.logging.Logger;
 
 public class ConnectionAcceptor implements Runnable {
@@ -12,9 +12,10 @@ public class ConnectionAcceptor implements Runnable {
 
     private ServerSocketChannel serverChannel;
     private Selector[] selectors;
-    private Queue<SocketChannel> inboundSockets;
+    private ArrayBlockingQueue<SocketChannel> inboundSockets;
 
-    ConnectionAcceptor(ServerSocketChannel serverChannel, Selector[] selectors, Queue<SocketChannel> inboundSockets) {
+    ConnectionAcceptor(ServerSocketChannel serverChannel, Selector[] selectors,
+                       ArrayBlockingQueue<SocketChannel> inboundSockets) {
         this.serverChannel = serverChannel;
         this.selectors = selectors;
         this.inboundSockets = inboundSockets;
@@ -25,11 +26,17 @@ public class ConnectionAcceptor implements Runnable {
         while (serverChannel.isOpen()) {
             try {
                 SocketChannel channel = serverChannel.accept();
-                inboundSockets.add(channel);
+                LOGGER.info("Connection from " + channel.getRemoteAddress().toString());
+                try {
+                    inboundSockets.put(channel);
+                } catch (InterruptedException e) {
+                    LOGGER.warning("Interrupted while trying to put inbound connection. Connection refused");
+                    channel.close();
+                    continue;
+                }
                 for (Selector selector : selectors) {
                     selector.wakeup();
                 }
-                LOGGER.info("Connection from " + channel.getRemoteAddress().toString());
             } catch (IOException e) {
                 LOGGER.warning("Error accepting connection");
             }
